516b25abb95591da1876b79ec199aa861648d2e8
Sneha <snehadeepika02@gmail.com>
2026-03-11 19:01:08 +0530

    Database connection added

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const db = require("./db");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Library Catalog Backend Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
